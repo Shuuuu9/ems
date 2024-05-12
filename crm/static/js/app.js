f95092e3ec511d2d06b5a2ba83706fadecad3c54
unknown <softypap@gmail.com>
2024-05-12 20:37:31 +0530

    adding all files

---
+++ crm/static/js/app.js
@@ -0,0 +1,25 @@
+
+// Message/Notification Timer---------------------------------------
+
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display = "none";
+
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ crm/webapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Record(models.Model):
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    email = models.CharField(max_length=255)
+
+    phone = models.CharField(max_length=10)
+
+    address = models.CharField(max_length=300)
+
+    city = models.CharField(max_length=50)
+
+    state = models.CharField(max_length=100)
+
+    country = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        
+        return self.first_name + "   " + self.last_name
+
+
+
+
+
+
+
